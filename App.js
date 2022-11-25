e67f10db877b8c8284dfb4b7e9036538a8b8c121
Chandru1106 <118824376+Chandru1106@users.noreply.github.com>
2022-11-25 16:21:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+//import logo from './logo.svg';
+/*import './App.css';
+import Pro1 from './Pro1'
+import Pro2 from './Pro2'
+import Pro3 from './Pro3'
+import Drainage from './Drainage'
+import Electricity from './Electricity'
+import Water from './Water'
+import Road from './Road'
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Pro1}></Route>
+        <Route exact path="/Pro2" component={Pro2}></Route>
+        <Route exact path="/Pro3" component={Pro3}></Route>
+        <Route exact path="/Drainage" component={Drainage}></Route>
+        <Route exact path="/Water" component={Water}></Route>
+        <Route exact path="/Electricity" component={Electricity}></Route>
+        <Route exact path="/Road" component={Road}></Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+export default App;*/
+
+import Ca1 from './Ca1';
+function App() {
+  return (
+    <div className="App">
+      <Ca1 />
+    </div>
+  )
+}
+export default App;
